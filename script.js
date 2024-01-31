71375af7a4d18f24eb05a17774b71d5ff1d090d4
defnekaynak <149432075+defnekaynak@users.noreply.github.com>
2024-01-31 14:52:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-// Mouse trail js below  -- https://devbeep.com/mouse-trail-effects/
-
-
-
-// dots is an array of Dot objects,
-// mouse is an object used to track the X and Y position
-   // of the mouse, set with a mousemove event listener below
 var dots = [],
     mouse = {
       x: 0,
       y: 0
     };
 
-// The Dot object used to scaffold the dots
 var Dot = function() {
   this.x = 0;
   this.y = 0;
@@ -22,27 +14,22 @@ var Dot = function() {
     return n;
   }());
 };
-// The Dot.prototype.draw() method sets the position of 
-  // the object's <div> node
+
 Dot.prototype.draw = function() {
   this.node.style.left = this.x + "px";
   this.node.style.top = this.y + "px";
 };
 
-// Creates the Dot objects, populates the dots array
+
 for (var i = 0; i < 200; i++) {
   var d = new Dot();
   dots.push(d);
 }
 
-// This is the screen redraw function
 function draw() {
-  // Make sure the mouse position is set everytime
-    // draw() is called.
   var x = mouse.x,
       y = mouse.y;
   
-  // This loop is where all the 90s magic happens
   dots.forEach(function(dot, index, dots) {
     var nextDot = dots[index + 1] || dots[0];
     
@@ -61,12 +48,9 @@ addEventListener("mousemove", function(event) {
   mouse.y = event.pageY;
 });
 
-// animate() calls draw() then recursively calls itself
-  // everytime the screen repaints via requestAnimationFrame().
 function animate() {
   draw();
   requestAnimationFrame(animate);
 }
 
-// And get it started by calling animate().
 animate();
